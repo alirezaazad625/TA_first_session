d5d8505d3801bfb9f50015915b9a54f67aa62b32
Alireza Azad <alirezaazad625@gmail.com>
2023-08-20 18:44:20 +0330

    add for.sh

---
+++ for.sh
@@ -0,0 +1,32 @@
+n=10
+
+for (( i = 0; i < n; i++ )); do
+    echo "1st for $i"
+done
+
+#for i in {0..10..2} ; do
+#    echo "2nd for $i"
+#done
+#
+#for i in {2,6,7,10}; do
+#  echo "3rd for $i"
+#done
+#
+#for i in 13 16 100; do
+#  echo "4th for $i"
+#done
+
+#for f in $( ls /var/ );
+#do
+#  echo "5th for $f"
+#done
+
+#for f in `ls`;
+#do
+#  echo "6th for $f"
+#done
+
+#for f in ./*;
+#do
+#  echo "7th for $f"
+#done
